717250a91ee9696a49da7ebe01418ea7ad0b362e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-18 15:30:44 +0900

    9.4 Sign Up UI

--- src/components/shared.js
+++ src/components/shared.js
@@ -5,3 +5,8 @@ export const BaseBox = styled.div`
   border: 1px solid ${(props) => props.theme.borderColor};
   width: 100%;
 `;
+
+export const FatLink = styled.span`
+  font-weight: 600;
+  color: rgb(142, 142, 142);
+`;
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,4 +1,46 @@
-function SignUp() {
-  return null;
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import AuthLayout from "../components/auth/AuthLayout";
+import BottomBox from "../components/auth/BottomBox";
+import Button from "../components/auth/Button";
+import FormBox from "../components/auth/FormBox";
+import Input from "../components/auth/Input";
+import { FatLink } from "../components/shared";
+import routes from "../routes";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Subtitle = styled(FatLink)`
+  font-size: 16px;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+function SingUp() {
+  return (
+    <AuthLayout>
+      <FormBox>
+        <HeaderContainer>
+          <FontAwesomeIcon icon={faInstagram} size="3x" />
+          <Subtitle>
+            Sign up to see photos and videos from your friends.
+          </Subtitle>
+        </HeaderContainer>
+        <form>
+          <Input type="text" placeholder="Name" />
+          <Input type="text" placeholder="Email" />
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Password" />
+          <Button type="submit" value="Sign up" />
+        </form>
+      </FormBox>
+      <BottomBox cta="Have an account?" linkText="Log in" link={routes.home} />
+    </AuthLayout>
+  );
 }
-export default SignUp;
+export default SingUp;
