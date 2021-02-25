0eb5e6b4e0bc63270f571b4b4eb2d4f993128c0f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-25 20:31:48 +0900

    10.5 Photo Component part Two

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 const SAvatar = styled.div`
-  width: 25px;
-  height: 25px;
-  border-radius: 15px;
+  width: ${(props) => (props.lg ? "30px" : "25px")};
+  height: ${(props) => (props.lg ? "30px" : "25px")};
+  border-radius: 50%;
   background-color: #2c2c2c;
   overflow: hidden;
 `;
@@ -12,7 +12,7 @@ const Img = styled.img`
   max-width: 100%;
 `;
 
-function Avatar({ url = "" }) {
-  return <SAvatar>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
+function Avatar({ url = "", lg = false }) {
+  return <SAvatar lg={lg}>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
 }
 export default Avatar;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,11 @@
 import { gql, useQuery } from "@apollo/client";
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { logUserOut } from "../apollo";
 import Avatar from "../components/Avatar";
@@ -26,15 +33,44 @@ const PhotoContainer = styled.div`
   background-color: white;
   border: 1px solid ${(props) => props.theme.borderColor};
   margin-bottom: 20px;
+  max-width: 615px;
 `;
 const PhotoHeader = styled.div`
-  padding: 5px 10px;
+  padding: 15px;
   display: flex;
   align-items: center;
 `;
 
 const Username = styled(FatText)`
-  margin-left: 5px;
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+  min-width: 100%;
+`;
+
+const PhotoData = styled.div`
+  padding: 15px;
+`;
+
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const PhotoAction = styled.div`
+  margin-right: 10px;
+`;
+
+const Likes = styled(FatText)`
+  margin-top: 15px;
+  display: block;
 `;
 
 function Home() {
@@ -44,9 +80,31 @@ function Home() {
       {data?.seeFeed?.map((photo) => (
         <PhotoContainer key={photo.id}>
           <PhotoHeader>
-            <Avatar url={photo.user.avatar} />
+            <Avatar lg url={photo.user.avatar} />
             <Username>{photo.user.username}</Username>
           </PhotoHeader>
+          <PhotoFile src={photo.file} />
+          <PhotoData>
+            <PhotoActions>
+              <div>
+                <PhotoAction>
+                  <FontAwesomeIcon size={"2x"} icon={faHeart} />
+                </PhotoAction>
+                <PhotoAction>
+                  <FontAwesomeIcon size={"2x"} icon={faComment} />
+                </PhotoAction>
+                <PhotoAction>
+                  <FontAwesomeIcon size={"2x"} icon={faPaperPlane} />
+                </PhotoAction>
+              </div>
+              <div>
+                <FontAwesomeIcon size={"2x"} icon={faBookmark} />
+              </div>
+            </PhotoActions>
+            <Likes>
+              {photo.likes === 1 ? "1 like" : `${photo.likes} likes`}
+            </Likes>
+          </PhotoData>
         </PhotoContainer>
       ))}
     </div>
