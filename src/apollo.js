8428fd440198764d9e98d485e169de43a0f66212
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-25 19:36:54 +0900

    10.2 Header part Three

--- src/apollo.js
+++ src/apollo.js
@@ -1,4 +1,10 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const TOKEN = "TOKEN";
 const DARK_MODE = "DARK_MODE";
@@ -27,7 +33,20 @@ export const disableDarkMode = () => {
   darkModeVar(false);
 };
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: localStorage.getItem(TOKEN),
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,5 +1,6 @@
 import { gql, useQuery, useReactiveVar } from "@apollo/client";
-import { isLoggedInVar } from "../apollo";
+import { useEffect } from "react";
+import { isLoggedInVar, logUserOut } from "../apollo";
 
 const ME_QUERY = gql`
   query me {
@@ -11,11 +12,16 @@ const ME_QUERY = gql`
 `;
 
 function useUser() {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
-  const { data, error } = useQuery(ME_QUERY, {
-    skip: !isLoggedIn,
+  const hasToken = useReactiveVar(isLoggedInVar);
+  const { data } = useQuery(ME_QUERY, {
+    skip: !hasToken,
   });
-  console.log(data, error);
+  console.log(data);
+  useEffect(() => {
+    if (data?.me === null) {
+      logUserOut();
+    }
+  }, [data]);
   return;
 }
 export default useUser;
