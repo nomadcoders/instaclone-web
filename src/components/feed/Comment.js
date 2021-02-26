fb5e138c21b21909c238d6f8658538a5f0942b3a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-26 20:09:37 +0900

    10.13 Parsing Hashtags

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,3 +1,4 @@
+import sanitizeHtml from "sanitize-html";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
@@ -5,13 +6,31 @@ import { FatText } from "../shared";
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 function Comment({ author, payload }) {
+  const cleanedPayload = sanitizeHtml(
+    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
+    {
+      allowedTags: ["mark"],
+    }
+  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
     </CommentContainer>
   );
 }
